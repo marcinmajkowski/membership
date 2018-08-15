5f97d3c2514f7bd23c382a37e7c7b39da9ce46e3
Marcin Majkowski <pl.marcin.m@gmail.com>
2018-08-15 18:49:23 +0200

    Add missing @NotNull annotation in CreatePaymentForm

--- src/main/java/com/marcinmajkowski/membership/payment/CreatePaymentForm.java
+++ src/main/java/com/marcinmajkowski/membership/payment/CreatePaymentForm.java
@@ -1,9 +1,11 @@
 package com.marcinmajkowski.membership.payment;
 
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 
 class CreatePaymentForm {
 
+    @NotNull
     private BigDecimal amount;
 
     public BigDecimal getAmount() {
